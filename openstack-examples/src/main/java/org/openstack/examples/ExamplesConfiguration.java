bcfbd07aa0d2262546de349ed05b43f224b0ac67
Nicolas Rivierre <nicolas.rivierre@orange.com>
2013-06-04 12:34:37 +0200

    synch with woorea

--- openstack-examples/src/main/java/org/openstack/examples/ExamplesConfiguration.java
+++ openstack-examples/src/main/java/org/openstack/examples/ExamplesConfiguration.java
@@ -20,13 +20,29 @@ public class ExamplesConfiguration {
 	
 	public static final String CEILOMETER_ENDPOINT = "";
 	
-    // -- Lannion
-    public static final String KEYSTONE_AUTH_URL = "http://10.194.138.42:5000/v2.0";
+    // -- Lannion v1
+    /*public static final String KEYSTONE_AUTH_URL = "http://10.194.138.42:5000/v2.0";
     //public static final String KEYSTONE_USERNAME = "lov_admin";
     public static final String KEYSTONE_USERNAME = "lov_user";
     public static final String KEYSTONE_PASSWORD = "20130305";
     
-    public static final String NOVA_ENDPOINT = "http://10.194.138.42:8774/v2";
+    public static final String NOVA_ENDPOINT = "http://10.194.138.42:8774/v2";*/
+	
+    // -- Lannion v2
+    /*public static final String KEYSTONE_AUTH_URL = "http://10.192.133.191:5000/v2.0";
+    //public static final String KEYSTONE_USERNAME = "lov_admin";
+    public static final String KEYSTONE_USERNAME = "lov_user";
+    public static final String KEYSTONE_PASSWORD = "lov_pass";
+    
+    public static final String NOVA_ENDPOINT = "http://10.192.133.191:8774/v2";*/
+	
+    // -- Fred local
+    public static final String KEYSTONE_AUTH_URL = "http://10.193.137.119:5000/v2.0";
+    //public static final String KEYSTONE_USERNAME = "admin";
+    public static final String KEYSTONE_USERNAME = "demo";
+    public static final String KEYSTONE_PASSWORD = "password";
+    
+    public static final String NOVA_ENDPOINT = "http://10.193.137.119:8774/v2";
 	
     // -- enocloud
     /*public static final String KEYSTONE_AUTH_URL = "http://ca.enocloud.com:5000/v2.0";
